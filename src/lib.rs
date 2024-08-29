34712a6fc43274c095b124b9dc9b0823b5429b70
Klim Tsoutsman <klim@tsoutsman.com>
2024-08-29 23:38:13 +1000

    Add `delete` and `update` for Apple platforms
    Reworked API to use methods on `Identifier` rather than standalone
    functions.
    
    This commit also temporarily breaks non-Apple platforms.
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- src/lib.rs
+++ src/lib.rs
@@ -36,15 +36,12 @@ pub use error::{Error, Result};
 /// Builder to add an item to the keychain.
 ///
 /// See the crate-level docs for more information.
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct KeychainItemBuilder<'a> {
     service: &'a str,
     secret: &'a str,
     class: Class,
     username: Option<&'a str>,
-    synchronizable: Option<bool>,
-    // comment: Option<&'a str>,
-    // attributes: Vec<(&'a str, &'a [u8])>,
 }
 
 /// The class of the keychain item.
@@ -68,7 +65,6 @@ impl<'a> KeychainItemBuilder<'a> {
             secret,
             class: Class::default(),
             username: None,
-            synchronizable: None,
         }
     }
 
@@ -84,22 +80,6 @@ impl<'a> KeychainItemBuilder<'a> {
         self
     }
 
-    pub fn synchronizable(mut self, synchronizable: bool) -> Self {
-        self.synchronizable = Some(synchronizable);
-        self
-    }
-
-    // pub fn comment(&mut self, comment: &'a str) {
-    //     self.comment = comment;
-    // }
-
-    // pub fn add_attribute<T>(&mut self, key: &'a str, value: &'a T)
-    // where
-    //     T: AsRef<[u8]>,
-    // {
-    //     self.attributes.push((key, value.as_ref()));
-    // }
-
     /// Stores the item in the keychain.
     pub fn store(self) -> Result<Identifier<'a>> {
         let identifier = Identifier {
@@ -112,45 +92,110 @@ impl<'a> KeychainItemBuilder<'a> {
     }
 }
 
+#[derive(Clone, Debug, Default)]
+pub struct UpdateOptions<'a> {
+    service: Option<&'a str>,
+    secret: Option<&'a str>,
+    class: Option<Class>,
+    username: Option<&'a str>,
+}
+
+impl<'a> UpdateOptions<'a> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Sets the service for the keychain item.
+    pub fn service(mut self, service: &'a str) -> Self {
+        self.service = Some(service);
+        self
+    }
+
+    /// Sets the secret for the keychain item.
+    pub fn secret(mut self, secret: &'a str) -> Self {
+        self.secret = Some(secret);
+        self
+    }
+
+    /// Sets the class for the keychain item.
+    pub fn class(mut self, class: Class) -> Self {
+        self.class = Some(class);
+        self
+    }
+
+    /// Sets the username for the keychain item.
+    pub fn username(mut self, username: &'a str) -> Self {
+        self.username = Some(username);
+        self
+    }
+}
+
 // An identifier returned by [`KeychainItemBuilder::store`] to later [`load`]
 // the item.
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct Identifier<'a> {
     pub service: &'a str,
     pub username: Option<&'a str>,
     pub class: Class,
 }
 
-// TODO: Add owned version of identifier for less hassle. It doesn't really work
-// with traits though because we can't return a reference to an `Identifier`.
+impl<'a> Identifier<'a> {
+    // Poor man's `ToOwned` impl. We can't impl ToOwned because OwnedIdentifier
+    // can't implement Borrow.
+    pub fn to_owned(&self) -> OwnedIdentifier {
+        OwnedIdentifier {
+            service: self.service.to_owned(),
+            username: self.username.map(ToOwned::to_owned),
+            class: self.class,
+        }
+    }
 
-// impl<'a> borrow::ToOwned for Identifier<'a> {
-//     type Owned = OwnedIdentifier;
-//
-//     fn to_owned(&self) -> Self::Owned {
-//         OwnedIdentifier {
-//             service: self.to_owned(),
-//             username: self.username.map(|username| username.to_owned()),
-//         }
-//     }
-// }
-//
-// pub struct OwnedIdentifier {
-//     pub service: String,
-//     pub username: String,
-// }
-//
-// impl<'a> borrow::Borrow<Identifier<'a>> for OwnedIdentifier {
-//     fn borrow(&self) -> &Identifier {
-//         &Identifier {
-//             service: self.service.borrow(),
-//             username: self.username.borrow(),
-//         }
-//     }
-// }
+    /// See the crate-level docs for more information.
+    pub fn load(&self) -> Result<Option<String>> {
+        sys::load(self)
+    }
 
-/// Loads an item from the keychain given an [`Identifier`].
-///
-/// See the crate-level docs for more information.
-pub fn load(identifier: Identifier) -> Result<Option<String>> {
-    sys::load(identifier)
+    pub fn update(&self, options: UpdateOptions<'a>) -> Result<Self> {
+        let new_id = Self {
+            service: options.service.unwrap_or(self.service),
+            username: options.username.or(self.username),
+            class: options.class.unwrap_or(self.class),
+        };
+        sys::update(self, options)?;
+        Ok(new_id)
+    }
+
+    pub fn delete(&self) -> Result<()> {
+        sys::delete(self)
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct OwnedIdentifier {
+    pub service: String,
+    pub username: Option<String>,
+    pub class: Class,
+}
+
+impl OwnedIdentifier {
+    fn as_ref(&self) -> Identifier {
+        Identifier {
+            service: self.service.as_ref(),
+            username: self.username.as_ref().map(|u| u.as_ref()),
+            class: self.class,
+        }
+    }
+
+    /// See the crate-level docs for more information.
+    pub fn load(&self) -> Result<Option<String>> {
+        self.as_ref().load()
+    }
+
+    pub fn update<'a>(&'a self, options: UpdateOptions<'a>) -> Result<Identifier<'a>> {
+        self.as_ref().update(options)
+    }
+
+    pub fn delete(&self) -> Result<()> {
+        self.as_ref().delete()
+    }
 }
--- src/sys/apple.rs
+++ src/sys/apple.rs
@@ -1,16 +1,14 @@
 use core_foundation::data::CFData;
 use security_framework::item::{
-    add_item, ItemAddOptions, ItemAddValue, ItemClass, ItemSearchOptions, SearchResult,
+    add_item, delete_item, update_item, ItemAddOptions, ItemAddValue, ItemClass, ItemSearchOptions,
+    ItemUpdateOptions, SearchResult,
 };
 
-use crate::{Class, Error, Identifier, KeychainItemBuilder, Result};
+use crate::{Class, Error, Identifier, KeychainItemBuilder, Result, UpdateOptions};
 
 pub fn store(item: KeychainItemBuilder) -> Result<()> {
     let mut options = ItemAddOptions::new(ItemAddValue::Data {
-        class: match item.class {
-            Class::Generic => ItemClass::generic_password(),
-            Class::Internet => ItemClass::internet_password(),
-        },
+        class: convert_class(item.class),
         data: CFData::from_buffer(item.secret.as_ref()),
     });
     if let Some(username) = item.username {
@@ -21,15 +19,8 @@ pub fn store(item: KeychainItemBuilder) -> Result<()> {
     add_item(options.to_dictionary()).map_err(Error)
 }
 
-pub fn load(identifier: Identifier) -> Result<Option<String>> {
-    let mut options = ItemSearchOptions::new();
-    options.service(identifier.service);
-    if let Some(username) = identifier.username {
-        options.account(username);
-    }
-
-    Ok(options
-        .class(ItemClass::generic_password())
+pub fn load(identifier: &Identifier) -> Result<Option<String>> {
+    Ok(search_options(identifier)
         .load_data(true)
         .search()
         .map_err(Error)?
@@ -40,4 +31,53 @@ pub fn load(identifier: Identifier) -> Result<Option<String>> {
             // TODO: Log error?
             _ => None,
         }))
+}
+
+pub fn update(identifier: &Identifier, options: UpdateOptions) -> Result<()> {
+    let mut sf_options = ItemUpdateOptions::new();
+    if let Some(service) = options.service {
+        sf_options.set_service(service);
+    }
+    if let Some(secret) = options.secret {
+        sf_options
+            .set_value(ItemAddValue::Data {
+                // This is ignored as we set class to `None`.
+                class: ItemClass::generic_password(),
+                data: CFData::from_buffer(secret.as_ref()),
+            })
+            .set_class(None);
+    }
+    if let Some(class) = options.class {
+        sf_options.set_class(Some(convert_class(class)));
+    }
+    if let Some(username) = options.username {
+        sf_options.set_account_name(username);
+    }
+    update_item(
+        search_options(identifier).to_dictionary(),
+        sf_options.to_dictionary(),
+    )
+    .map_err(Error)
+}
+
+pub fn delete(identifier: &Identifier) -> Result<()> {
+    delete_item(search_options(identifier).to_dictionary()).map_err(Error)
+}
+
+fn search_options(identifier: &Identifier) -> ItemSearchOptions {
+    let mut options = ItemSearchOptions::new();
+    options
+        .service(identifier.service)
+        .class(convert_class(identifier.class));
+    if let Some(username) = identifier.username {
+        options.account(username);
+    }
+    options
+}
+
+fn convert_class(class: Class) -> ItemClass {
+    match class {
+        Class::Generic => ItemClass::generic_password(),
+        Class::Internet => ItemClass::internet_password(),
+    }
 }
